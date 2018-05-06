7c187c720fc6612be7f56bea6f63e4064f7d7c8c
Bharat Sinha <bharatsinha@Bharats-MacBook-Pro.local>
2018-05-06 13:36:04 +0530

    add messages

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fetch = require('request-promise');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,21 +9,14 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.json({ success: true });
+    fetch('http://api.yomomma.info/')
+    .then(data => res.send(data))
+    // res.json({ success: true });
 })
 .post('/', (req, res) => {
     let responseText = '';
     let shouldEndSession = true;
-    console.log(req.body);
-    if (req.body.request.type === 'LaunchRequest') {
-        responseText = 'Hey, I am here to make your day';
-        shouldEndSession = false;
-    }
-    else {
-        responseText = 'Hey, I am not that funny';
-        shouldEndSession = false;
-    }
-    const responseObj = {
+    let responseObj = {
         "version": "string",
         "sessionAttributes": {
           "key": "value"
@@ -30,12 +24,26 @@ app.get('/', (req, res) => {
         "response": {
           "outputSpeech": {
             "type": "PlainText",
-            "text": responseText,
+            "text": '',
             "ssml": "<speak>SSML text string to speak</speak>"
           },
-          shouldEndSession
+          shouldEndSession: false
         }
-      }
+    }
+    console.log(req.body);
+    if (req.body.request.type === 'LaunchRequest') {
+        responseObj.response.text = 'Hey, I am here to make your day';
+        responseObj.shouldEndSession = false;
+        res.json(responseObj);
+    }
+    else {
+        responseObj.shouldEndSession = false;
+        fetch('http://api.yomomma.info/')
+        .then(data => {
+            responseObj.response.text = data.joke;
+            res.json(responseObj);
+        });
+    }
       
     res.json(responseObj);
 });

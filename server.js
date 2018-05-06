71856a8cbc48d9926e243c52af65cb003247796c
Bharat Sinha <bharatsinha@Bharats-MacBook-Pro.local>
2018-05-06 12:33:07 +0530

    add post

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.json({ success: true });
+})
+.post('/', (req, res) => {
+    console.log(req);
+    res.json({post: 'success'});
 });
+
 
 app.listen(PORT, () => {
     console.log(`App is running on port no ${PORT}`);

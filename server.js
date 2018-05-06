afcbcad1fa159738e0587eaa9e1d0c7da019e47e
Bharat Sinha <bharatsinha@Bharats-MacBook-Pro.local>
2018-05-06 13:45:59 +0530

    yo mama jokes

--- server.js
+++ server.js
@@ -31,17 +31,17 @@ app.get('/', (req, res) => {
         }
     }
     if (req.body.request.type === 'LaunchRequest') {
-        responseObj.response.text = 'Hey, I am here to make your day';
-        responseObj.shouldEndSession = false;
+        responseObj.response.outputSpeech.text = 'Hey, I am here to make your day';
+        responseObj.response.shouldEndSession = false;
         console.log('try m e', responseObj);
         res.json(responseObj);
     }
     else {
         console.log('here');
-        responseObj.shouldEndSession = false;
+        responseObj.response.shouldEndSession = false;
         fetch('http://api.yomomma.info/')
         .then(data => {
-            responseObj.response.text = data.joke;
+            responseObj.response.outputSpeech.text = data.joke;
             res.json(responseObj);
         });
     }

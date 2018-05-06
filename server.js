5f46b3f7e69ab71b76741a8812e93175613205ae
Bharat Sinha <bharatsinha@Bharats-MacBook-Pro.local>
2018-05-06 12:59:23 +0530

    add body parser

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.json({ success: true });
